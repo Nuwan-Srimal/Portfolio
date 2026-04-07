eb5060953f5a38e7aed7a9b981b1d525d9d58187
Nuwan Srimal <112359694+Nuwan-Srimal@users.noreply.github.com>
2026-04-07 17:00:58 +0530

    deploy vercel✏️

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import '../style.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
